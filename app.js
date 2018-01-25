6867a6d48872067c9a58d360e8078e5fced352c1
房召谋 <1079336302@qq.com>
2018-01-25 10:42:12 +0800

    test

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+	host     : '176.122.128.91',
+	user     : 'fzm',
+	password : '123456',
+	database: 'fzm',
+	port: 3306
+});
+connection.connect();
+
+
+app.get('/', function (req, res) {
+	connection.query('SELECT * from user;', function(err, rows, fields) {
+		if (err) throw err;
+		console.log(rows)
+		res.send(rows)
+	});
+});
+
+var server = app.listen(3000, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Example app listening at http://%s:%s', host, port);
+});
